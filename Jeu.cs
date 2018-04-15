20426b7e78d3b2409d32c5c49d0dbca67c266b84
gillesveyet <34187169+gillesveyet@users.noreply.github.com>
2018-04-16 00:16:40 +0200

    Step #6

--- Jeu.cs
+++ Jeu.cs
@@ -2,39 +2,57 @@
 using IrrlichtLime.Core;
 using IrrlichtLime.Scene;
 using IrrlichtLime.Video;
+using System.Drawing;
 
 namespace Canardstein
 {
-    public class Jeu
-    {
-        private static void Main(string[] args) { Jeu jeu = new Jeu(); }
+	public class Jeu
+	{
+		private static void Main(string[] args) { Jeu jeu = new Jeu(); }
 
-        private IrrlichtDevice Device;
-        private uint DerniereFrame = 0;
-        private bool K_Avant, K_Arriere, K_Gauche, K_Droite;
-		private Texture TextureMur, TextureSol, TexturePlafond;
+		private IrrlichtDevice Device;
+		private uint DerniereFrame = 0;
+		private bool K_Avant, K_Arriere, K_Gauche, K_Droite;
+		private Texture TextureMur, TextureMurDeco, TextureSol, TexturePlafond;
 
 		public Jeu()
-        {
-            Device = IrrlichtDevice.CreateDevice(
-                DriverType.Direct3D9,
-                new Dimension2Di(800, 600),
-                32, false, false, true);
+		{
+			Device = IrrlichtDevice.CreateDevice(
+				DriverType.Direct3D9,
+				new Dimension2Di(800, 600),
+				32, false, false, true);
 
-            Device.SetWindowCaption("Canardstein 3D");
-            Device.OnEvent += Evenement;
+			Device.SetWindowCaption("Canardstein 3D");
+			Device.OnEvent += Evenement;
 
 			TextureMur = Device.VideoDriver.GetTexture(@"Textures\mur.png");
+			TextureMurDeco = Device.VideoDriver.GetTexture(@"Textures\mur_deco.png");
 			TextureSol = Device.VideoDriver.GetTexture(@"Textures\sol.png");
 			TexturePlafond = Device.VideoDriver.GetTexture(@"Textures\plafond.png");
 
-			//Ajouter un cube dont les côtés sont de taille 1, qui n'est rattaché à aucun autre objet (null), identifié par le numéro 0, aux coordonnées 2,0,0, avec une rotation de 45 degrés sur l'axe Y ;
-			SceneNode cube = Device.SceneManager.AddCubeSceneNode(1, null, 0, new Vector3Df(2, 0, 0), new Vector3Df(0, 45, 0));
+			using (Bitmap carte = (Bitmap)System.Drawing.Image.FromFile(@"Textures\carte.png"))
+			{
+				for (int x = 0; x < 32; x++)
+					for (int y = 0; y < 32; y++)
+					{
+						System.Drawing.Color col = carte.GetPixel(x, y);
 
-			cube.SetMaterialTexture(0, TextureMur);
+						if ((col.R == 255) && (col.G == 255) && (col.B == 255))
+						{
+							SceneNode cube = Device.SceneManager.AddCubeSceneNode(1, null, 0, new Vector3Df(x, 0, y));
+							cube.SetMaterialFlag(MaterialFlag.Lighting, false);
+							cube.SetMaterialTexture(0, TextureMur);
+						}
+						else if ((col.R == 0) && (col.G == 0) && (col.B == 255))
+						{
+							SceneNode cube = Device.SceneManager.AddCubeSceneNode(1, null, 0, new Vector3Df(x, 0, y));
+							cube.SetMaterialFlag(MaterialFlag.Lighting, false);
+							cube.SetMaterialTexture(0, TextureMurDeco);
+						}
+					}
+			}
 
-			//Désactiver l'éclairage du cube (sans quoi il serait tout noir, vu qu'on n'a aucune source lumineuse) 
-			cube.SetMaterialFlag(MaterialFlag.Lighting, false);
+
 
 			//On va maintenant créer un plancher et un plafond, histoire de mettre un peu d'ordre. On commence par générer meshSol,
 			//le modèle 3D d'un plan que nous allons nommer « plan » (parce qu'il est obligatoire de fournir un nom), divisé en 32 × 32 cases de taille 1 × 1.
@@ -58,53 +76,52 @@ namespace Canardstein
 			plafond.Position = new Vector3Df(15.5f, 0.5f, 15.5f);
 			plafond.Rotation = new Vector3Df(180, 0, 0);
 
-			//Ajouter une caméra attachée à aucun autre objet (null), aux coordonnées 0,0,0, tournée vers les coordonnées 2,0,0 (celles du cube).
-			CameraSceneNode camera = Device.SceneManager.AddCameraSceneNode(null, new Vector3Df(0, 0, 0), new Vector3Df(2, 0, 0));
+			CameraSceneNode camera = Device.SceneManager.AddCameraSceneNode(null, new Vector3Df(1, 0, 1), new Vector3Df(2, 0, 1));
 
 			//Abaisse la distance minimum d'affichage de la caméra. La valeur par défaut est de 1, ce qui ne nous convient pas :
 			//plafond et sol se trouvant à 0,5 unité de la caméra, ils seraient trop près pour être dessinés.
 			camera.NearValue = 0.1f;
 
 			while (Device.Run())
-            {
-                float tempsEcoule = (Device.Timer.Time - DerniereFrame) / 1000f;
-                DerniereFrame = Device.Timer.Time;
+			{
+				float tempsEcoule = (Device.Timer.Time - DerniereFrame) / 1000f;
+				DerniereFrame = Device.Timer.Time;
 
-                Vector3Df vitesse = new Vector3Df();
+				Vector3Df vitesse = new Vector3Df();
 
-                if (K_Avant)
-                    vitesse.X = 1;
-                else if (K_Arriere)
-                    vitesse.X = -1;
-                if (K_Gauche)
-                    vitesse.Z = 1;
-                else if (K_Droite)
-                    vitesse.Z = -1;
+				if (K_Avant)
+					vitesse.X = 1;
+				else if (K_Arriere)
+					vitesse.X = -1;
+				if (K_Gauche)
+					vitesse.Z = 1;
+				else if (K_Droite)
+					vitesse.Z = -1;
 
-                vitesse = vitesse.Normalize() * tempsEcoule * 2;
-                camera.Position += vitesse;
-                camera.Target = camera.Position + new Vector3Df(1, 0, 0);
+				vitesse = vitesse.Normalize() * tempsEcoule * 2;
+				camera.Position += vitesse;
+				camera.Target = camera.Position + new Vector3Df(1, 0, 0);
 
-                Device.VideoDriver.BeginScene(ClearBufferFlag.Color | ClearBufferFlag.Depth, Color.OpaqueMagenta);
-                Device.SceneManager.DrawAll();
-                Device.VideoDriver.EndScene();
-            }
-        }
+				Device.VideoDriver.BeginScene(ClearBufferFlag.Color | ClearBufferFlag.Depth, IrrlichtLime.Video.Color.OpaqueMagenta);
+				Device.SceneManager.DrawAll();
+				Device.VideoDriver.EndScene();
+			}
+		}
 
-        private bool Evenement(Event e)
-        {
-            if (e.Type == EventType.Key)
-            {
-                switch (e.Key.Key)
-                {
-                    case KeyCode.KeyZ: K_Avant = e.Key.PressedDown; break;
-                    case KeyCode.KeyS: K_Arriere = e.Key.PressedDown; break;
-                    case KeyCode.KeyQ: K_Gauche = e.Key.PressedDown; break;
-                    case KeyCode.KeyD: K_Droite = e.Key.PressedDown; break;
-                }
-            }
-            return false;
-        }
-    }
+		private bool Evenement(Event e)
+		{
+			if (e.Type == EventType.Key)
+			{
+				switch (e.Key.Key)
+				{
+					case KeyCode.KeyZ: K_Avant = e.Key.PressedDown; break;
+					case KeyCode.KeyS: K_Arriere = e.Key.PressedDown; break;
+					case KeyCode.KeyQ: K_Gauche = e.Key.PressedDown; break;
+					case KeyCode.KeyD: K_Droite = e.Key.PressedDown; break;
+				}
+			}
+			return false;
+		}
+	}
 }
 
